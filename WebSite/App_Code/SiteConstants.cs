bfb56413483dbd6f0bc62b5f064223d1bbd11d38
Matt Perdeck <matt.perdeck@readify.net>
2013-11-03 23:30:07 +1100

    Created jsonly version of get started page

---
+++ WebSite/App_Code/SiteConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.App_Code
+{
+    public static class SiteConstants
+    {
+        public const string CurrentVersion = "2.3.0";
+        public const string JsnlogJsFileSize = "1.5kb";
+
+        public static string DownloadLinkJsnlogJs 
+        {
+            get { return string.Format("https://raw.github.com/mperdeck/jsnlog/{0}/JSNLog/Scripts/jsnlog.min.js", CurrentVersion); }
+        }
+    }
+}
+
