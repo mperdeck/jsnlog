677b6eff27e19415fb0ed8cfc6249dc8ecdb1fea
mperdeck <mperdeck@gmail.com>
2014-03-03 22:58:48 +1100

    Added page in directory in test site
    Used to test whether .logger url is recognised by the server side
    component

---
+++ EmptyTestSites/EmptyNLog/Controllers/Level1Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+
+namespace EmptyLog4Net.Controllers
+{
+    public class Level1Controller : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Page1()
+        {
+            Logger logger = LogManager.GetLogger("serverlogger");
+            logger.Warn("Warn Message generated on server, Level1/Page1");
+
+            return View();
+        }
+
+    }
+}
