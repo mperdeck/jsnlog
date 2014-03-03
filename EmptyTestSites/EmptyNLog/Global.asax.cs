056d00520ec65cbb0d1ea5d08c66719766cd0bad
mperdeck <mperdeck@gmail.com>
2014-03-03 22:56:25 +1100

    Fixed dll version issues in empty test sites

--- EmptyTestSites/EmptyNLog/Global.asax.cs
+++ EmptyTestSites/EmptyNLog/Global.asax.cs
@@ -18,7 +18,6 @@ namespace EmptyNLog
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
--- EmptyTestSites/Readme.txt
+++ EmptyTestSites/Readme.txt
@@ -12,3 +12,7 @@ The site it then loaded in a browser and the logs examined to see if they are as
 
 The other sites are very simple sites that log 1 entry, using log4net, nlog, elmah.
 Copy to other directory, and use to make sure that manual installation works well.
+
+========================
+
+All the MVC sites use MVC 5.
