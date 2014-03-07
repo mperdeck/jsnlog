87dd1401057677cf10be14736b4898b0036b38e2
mperdeck <mperdeck@gmail.com>
2014-03-07 22:14:35 +1100

    2.4.10

--- WebSite/App_Code/Generated.cs
+++ WebSite/App_Code/Generated.cs
@@ -1,11 +1,11 @@
 // Generated.cs gets copied from Generated.cs.template
-// During the copy, 2.4.9 is replaced by the actual version.
+// During the copy, 2.4.10 is replaced by the actual version.
 
 namespace WebSite.App_Code
 {
     public static class Generated
     {
-        public static string Version { get { return "2.4.9";  } }
+        public static string Version { get { return "2.4.10";  } }
     }
 }
 
