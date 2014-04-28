f1a0e688604955b864935fe3215950e90e564701
mperdeck <mperdeck@gmail.com>
2014-04-28 22:27:11 +1000

    Added twitter and fb buttons

--- WebSite/App_Code/Generated.cs
+++ WebSite/App_Code/Generated.cs
@@ -1,11 +1,11 @@
 // Generated.cs gets copied from Generated.cs.template
-// During the copy, 2.6.8 is replaced by the actual version.
+// During the copy, 2.6.9 is replaced by the actual version.
 
 namespace WebSite.App_Code
 {
     public static class Generated
     {
-        public static string Version { get { return "2.6.8";  } }
+        public static string Version { get { return "2.6.9";  } }
     }
 }
 
