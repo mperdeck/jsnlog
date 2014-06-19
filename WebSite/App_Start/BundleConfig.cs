5bd131b4011c4ccbeaf419672e555e094f3c3218
mperdeck <mperdeck@gmail.com>
2014-06-19 23:17:47 +1000

    2.7.1

--- WebSite/App_Start/BundleConfig.cs
+++ WebSite/App_Start/BundleConfig.cs
@@ -36,54 +36,19 @@ namespace WebSite
 
             // --------------------
 
-            var lessBundle = new StyleBundle("~/Content/bootstrap");
-            lessBundle.Include("~/Content/bootstrap-3.1.1/bootstrap.less");
-            lessBundle.Transforms.Add(new CssTransformer());
-            lessBundle.Transforms.Add(new CssMinify());
-            bundles.Add(lessBundle);
+            // Does not work on live host. Throws
+            // Could not load file or assembly 'ClearScriptV8-32.dll' or one of its dependencies. Access is denied.
+            //var lessBundle = new StyleBundle("~/Content/bootstrap");
+            //lessBundle.Include("~/Content/bootstrap-3.1.1/bootstrap.less");
+            //lessBundle.Transforms.Add(new CssTransformer());
+            //lessBundle.Transforms.Add(new CssMinify());
+            //bundles.Add(lessBundle);
 
 
-
-
-            
-        //        .Include("~/Content/bootstrap-3.1.1/grid.less")
-
-
-
-
-
-
-
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            //            "~/Scripts/jquery-ui-{version}.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.unobtrusive*",
-            //            "~/Scripts/jquery.validate*"));
-
-            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
-            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-            //            "~/Content/themes/base/jquery.ui.core.css",
-            //            "~/Content/themes/base/jquery.ui.resizable.css",
-            //            "~/Content/themes/base/jquery.ui.selectable.css",
-            //            "~/Content/themes/base/jquery.ui.accordion.css",
-            //            "~/Content/themes/base/jquery.ui.autocomplete.css",
-            //            "~/Content/themes/base/jquery.ui.button.css",
-            //            "~/Content/themes/base/jquery.ui.dialog.css",
-            //            "~/Content/themes/base/jquery.ui.slider.css",
-            //            "~/Content/themes/base/jquery.ui.tabs.css",
-            //            "~/Content/themes/base/jquery.ui.datepicker.css",
-            //            "~/Content/themes/base/jquery.ui.progressbar.css",
-            //            "~/Content/themes/base/jquery.ui.theme.css"));
+            var cssBundle = new StyleBundle("~/Content/css");
+            cssBundle.Include("~/Content/bootstrap-3.1.1/bootstrap.css");
+            cssBundle.Include("~/Content/Site.css");
+            bundles.Add(cssBundle);
         }
     }
 }
