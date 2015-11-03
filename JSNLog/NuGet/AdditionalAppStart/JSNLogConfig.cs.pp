8a7d01a401bbd73a64dbd7b5854855628e4ec788
Christoffer Petersen <cb.petersen@gmail.com>
2015-11-03 15:10:39 +0100

    align indentations
    converts tabs to spaces

--- JSNLog/NuGet/AdditionalAppStart/JSNLogConfig.cs.pp
+++ JSNLog/NuGet/AdditionalAppStart/JSNLogConfig.cs.pp
@@ -8,17 +8,16 @@ using System.Web.Mvc;
 namespace $rootnamespace$.App_Start {
     public static class JSNLogConfig {
         public static void PostStart() {
-            // Insert a route that ignores the jsnlog.logger route. That way, 
-			// requests for jsnlog.logger will get through to the handler defined
+            // Insert a route that ignores the jsnlog.logger route. That way,
+            // requests for jsnlog.logger will get through to the handler defined
             // in web.config.
-			//
-			// The route must take this particular form, including the constraint, 
-			// otherwise ActionLink will be confused by this route and generate the wrong URLs.
-			
+            //
+            // The route must take this particular form, including the constraint,
+            // otherwise ActionLink will be confused by this route and generate the wrong URLs.
+
             var jsnlogRoute = new Route("{*jsnloglogger}", new StopRoutingHandler());
             jsnlogRoute.Constraints = new RouteValueDictionary {{ "jsnloglogger", @"jsnlog\.logger(/.*)?" }};
             RouteTable.Routes.Insert(0, jsnlogRoute);
         }
     }
 }
-
