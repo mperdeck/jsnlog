0c1c5414d4ef80294429ca9bc445aceb8323b37e
mperdeck <mperdeck@gmail.com>
2015-11-26 22:18:15 +1100

    Added DemoTests test class, that also writes partials with all demo code to jsnlog.website

--- JSNLog.Tests/Logic/TestUtils.cs
+++ JSNLog.Tests/Logic/TestUtils.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Web;
 using System.Xml;
 using JSNLog.Infrastructure;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace JSNLog.Tests.Logic
 {
@@ -196,5 +200,137 @@ namespace JSNLog.Tests.Logic
 
             return js;
         }
+
+        /// <summary>
+        /// Evalutes the passed in string as C# code.
+        /// Returns the resulting value.
+        /// </summary>
+        /// <param name="sCSCode"></param>
+        /// <returns></returns>
+        public static object Eval(string sCSCode)
+        {
+            CSharpCodeProvider c = new CSharpCodeProvider();
+            ICodeCompiler icc = c.CreateCompiler();
+            CompilerParameters cp = new CompilerParameters();
+
+            cp.ReferencedAssemblies.Add("system.dll");
+            cp.ReferencedAssemblies.Add("system.xml.dll");
+            cp.ReferencedAssemblies.Add("JSNLog.dll");
+
+            cp.CompilerOptions = "/t:library";
+            cp.GenerateInMemory = true;
+
+            StringBuilder sb = new StringBuilder("");
+            sb.Append("using System;\n");
+            sb.Append("using System.Xml;\n");
+            sb.Append("using System.Collections.Generic;\n");
+            sb.Append("using JSNLog;\n");
+
+            sb.Append("namespace CSCodeEvaler{ \n");
+            sb.Append("public class CSCodeEvaler{ \n");
+            sb.Append("public object EvalCode(){\n");
+            sb.Append("return " + sCSCode + "; \n");
+            sb.Append("} \n");
+            sb.Append("} \n");
+            sb.Append("}\n");
+
+            CompilerResults cr = icc.CompileAssemblyFromSource(cp, sb.ToString());
+            if (cr.Errors.Count > 0)
+            {
+                throw new Exception("Error evaluating cs code: " + cr.Errors[0].ErrorText);
+            }
+
+            System.Reflection.Assembly a = cr.CompiledAssembly;
+            object o = a.CreateInstance("CSCodeEvaler.CSCodeEvaler");
+
+            Type t = o.GetType();
+            MethodInfo mi = t.GetMethod("EvalCode");
+
+            object s = mi.Invoke(o, null);
+            return s;
+        }
+
+        public static void EnsureEqualJsnlogConfiguration(JsnlogConfiguration jc1, JsnlogConfiguration jc2)
+        {
+            Assert.AreEqual(jc1.enabled, jc2.enabled);
+            Assert.AreEqual(jc1.maxMessages, jc2.maxMessages);
+            Assert.AreEqual(jc1.defaultAjaxUrl, jc2.defaultAjaxUrl);
+            Assert.AreEqual(jc1.corsAllowedOriginsRegex, jc2.corsAllowedOriginsRegex);
+            Assert.AreEqual(jc1.serverSideLogger, jc2.serverSideLogger);
+            Assert.AreEqual(jc1.serverSideLevel, jc2.serverSideLevel);
+            Assert.AreEqual(jc1.serverSideMessageFormat, jc2.serverSideMessageFormat);
+            Assert.AreEqual(jc1.dateFormat, jc2.dateFormat);
+            Assert.AreEqual(jc1.productionLibraryPath, jc2.productionLibraryPath);
+
+            EnsureListsEqual(jc1.ajaxAppenders, jc2.ajaxAppenders, EnsureEqualAjaxAppender);
+            EnsureListsEqual(jc1.consoleAppenders, jc2.consoleAppenders, EnsureEqualConsoleAppender);
+            EnsureListsEqual(jc1.loggers, jc2.loggers, EnsureEqualLogger);
+        }
+
+        public static void EnsureEqualAjaxAppender(AjaxAppender aa1, AjaxAppender aa2)
+        {
+            Assert.AreEqual(aa1.url, aa2.url);
+            EnsureEqualAppender(aa1, aa2);
+        }
+
+        public static void EnsureEqualConsoleAppender(ConsoleAppender ca1, ConsoleAppender ca2)
+        {
+            EnsureEqualAppender(ca1, ca2);
+        }
+
+        public static void EnsureEqualAppender(Appender a1, Appender a2)
+        {
+            Assert.AreEqual(a1.name, a2.name);
+            Assert.AreEqual(a1.sendWithBufferLevel, a2.sendWithBufferLevel);
+            Assert.AreEqual(a1.storeInBufferLevel, a2.storeInBufferLevel);
+            Assert.AreEqual(a1.bufferSize, a2.bufferSize);
+            Assert.AreEqual(a1.batchSize, a2.batchSize);
+
+            EnsureEqualFilterOptions(a1, a2);
+        }
+
+        public static void EnsureEqualLogger(Logger l1, Logger l2)
+        {
+            Assert.AreEqual(l1.appenders, l2.appenders);
+            Assert.AreEqual(l1.name, l2.name);
+
+            EnsureListsEqual(l1.onceOnlies, l2.onceOnlies, EnsureEqualOnceOnlyOptions);
+
+            EnsureEqualFilterOptions(l1, l2);
+        }
+
+        internal static void EnsureEqualFilterOptions(FilterOptions fc1, FilterOptions fc2)
+        {
+            Assert.AreEqual(fc1.level, fc2.level);
+            Assert.AreEqual(fc1.ipRegex, fc2.ipRegex);
+            Assert.AreEqual(fc1.userAgentRegex, fc2.userAgentRegex);
+            Assert.AreEqual(fc1.disallow, fc2.disallow);
+        }
+
+        public static void EnsureEqualOnceOnlyOptions(OnceOnlyOptions oo1, OnceOnlyOptions oo2)
+        {
+            Assert.AreEqual(oo1.regex, oo2.regex);
+        }
+
+        public static void EnsureListsEqual<T>(List<T> list1, List<T> list2, Action<T, T> ensurer)
+        {
+            // Ensure either both lists are null, or neither is null
+            Assert.AreEqual((list1 == null), (list2 == null));
+
+            // If list1 is null here, list2 is null as well.
+            if (list1 == null)
+            {
+                return;
+            }
+
+            int nbrElements = list1.Count;
+            Assert.AreEqual(nbrElements, list2.Count);
+
+            for(int i = 0; i < nbrElements; i++)
+            {
+                ensurer(list1[i], list2[i]);
+            }
+        }
+
     }
 }
---
+++ JSNLog.Tests/UnitTests/DemoTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSNLog.LogHandling;
+using JSNLog.Tests.Logic;
+using System.Xml;
+using JSNLog.Infrastructure;
+using System.Text;
+using JSNLog.Exceptions;
+using System.IO;
+
+namespace JSNLog.Tests.UnitTests
+{
+    [TestClass]
+    public class DemoTests
+    {
+        // All tests here write a partial with the complete demo html.
+        // The partials are written to directory:
+        private const string _demosDirectory = @"D:\Dev\JSNLog\jsnlog.website\WebSite\Views\Shared\Demos";
+
+        // >>>>>>>>>>>>>>>>>>>>>>>
+        // All files are removed from the Demos directory before the tests are run.
+        // If you run 1 test, you'll get only 1 file.
+        // Be sure to run ALL tests before publishing the web site.
+        // <<<<<<<<<<<<<<<<<<<<<<<<
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext a)
+        {
+            // Delete all files in Demos directory
+
+            System.IO.DirectoryInfo demosDirectory = new DirectoryInfo(_demosDirectory);
+
+            foreach (FileInfo file in demosDirectory.GetFiles("*.cshtml"))
+            {
+                file.Delete();
+            }
+        }
+
+        [TestMethod]
+        public void DemoTest1()
+        {
+            TestDemo(
+                @"
+<jsnlog serverSideMessageFormat=""Sent: % date, Brower: % userAgent - % message"" >
+</jsnlog>",
+                @"
+new JsnlogConfiguration {
+    serverSideMessageFormat=""Sent: % date, Brower: % userAgent - % message""
+}",
+                "jsnlog1");
+        }
+
+        /// <summary>
+        /// Ensures that the xml will be serialised by JSNLog to the code in csharp.
+        /// Also writes HTML to d:\temp\demos.html with premade html for example tabs.
+        /// </summary>
+        /// <param name="configXml"></param>
+        /// <param name="csharp"></param>
+        public void TestDemo(string configXml, string csharp, string demoId)
+        {
+            // Testing to ensure xml and code are the same
+
+            XmlElement xe = TestUtils.ConfigToXe(configXml);
+            var jsnlogConfigurationFromXml = XmlHelpers.DeserialiseXml<JsnlogConfiguration>(xe);
+
+            JsnlogConfiguration jsnlogConfigurationFromCode = (JsnlogConfiguration)TestUtils.Eval(csharp);
+
+            TestUtils.EnsureEqualJsnlogConfiguration(jsnlogConfigurationFromXml, jsnlogConfigurationFromCode);
+
+            // Write partial
+
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("@* GENERATED CODE - by class DemoTests in JSNLog.Tests project. Demo {0}. *@", demoId));
+
+            sb.AppendLine(@"<div class=""commontabs""><div data-tab=""Web.config"">");
+            sb.AppendLine(@"");
+            sb.AppendLine(string.Format(@"<pre>{0}</pre>", HttpUtility.HtmlEncode(configXml.Trim())));
+            sb.AppendLine(@"");
+            sb.AppendLine(@"</div><div data-tab=""JsnlogConfiguration"">");
+            sb.AppendLine(@"");
+            sb.AppendLine(string.Format(@"<pre>{0}</pre>", HttpUtility.HtmlEncode(csharp.Trim())));
+            sb.AppendLine(@"");
+            sb.AppendLine(@"</div></div>");
+
+            string path = Path.Combine(_demosDirectory, string.Format("_{0}.cshtml", demoId));
+            string content = sb.ToString();
+
+            bool fileExists = File.Exists(path);
+            Assert.IsFalse(fileExists, string.Format("{0} already exists", path));
+
+            System.IO.File.WriteAllText(path, content);
+        }
+
+    }
+}
+
