28733d92312f722407aa8f2d6802a64cb59121f1
mperdeck <mperdeck@gmail.com>
2015-12-05 15:51:06 +1100

    Initial aspnet5 additions

---
+++ JSNLog.aspnet5/PublicFacing/Configuration/Middleware/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.Builder;
+
+namespace JSNLog
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void UseJSNLog(this IApplicationBuilder app, string loggerUrlRegex = null)
+        {
+            app.Use<MiddlewareComponent>(loggerUrlRegex);
+        }
+    }
+}
