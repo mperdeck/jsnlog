a40c542224dd582ccec55844dc3490cec92eb6fd
mperdeck <mperdeck@gmail.com>
2016-01-13 00:12:20 +1100

    Implement mvc properly in the TestSite

--- src/JSNLog.TestSite/Startup.cs
+++ src/JSNLog.TestSite/Startup.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 
 namespace JSNLog.TestSite
 {
@@ -15,6 +17,7 @@ namespace JSNLog.TestSite
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,9 +25,13 @@ namespace JSNLog.TestSite
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
 
