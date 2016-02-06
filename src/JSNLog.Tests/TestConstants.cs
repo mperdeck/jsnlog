aa3b186a36de49e930459898549afa225af7962c
mperdeck <mperdeck@gmail.com>
2016-02-06 15:18:20 +1100

    Add TestConstants class

---
+++ src/JSNLog.Tests/TestConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JSNLog.Tests
+{
+    public class TestConstants
+    {
+        public const string _jsnlogDirectory = @"C:\Dev\JSNLog\";
+
+        // All tests here write a partial with the complete demo html.
+        // The partials are written to directory:
+        public const string _demosDirectory = _jsnlogDirectory + @"jsnlog.website\WebSite\Views\Shared\Demos";
+
+        public const string _jsnlogDllDirectory = _jsnlogDirectory + @"jsnlog\artifacts\bin\JSNLog\Debug\dnx451\JSNLog.dll";
+    }
+}
