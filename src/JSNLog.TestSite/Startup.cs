40d7c72001b37e315c547fdcfe0c6b6aab51b315
dazinator <darrell.tunnell@googlemail.com>
2016-06-17 18:38:35 +0100

    Fixes issue caused by IHttpContextAccessor no longer being registered by default: https://github.com/aspnet/Hosting/issues/793

--- src/JSNLog.TestSite/Startup.cs
+++ src/JSNLog.TestSite/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -17,6 +18,7 @@ namespace JSNLog.TestSite
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
         }
 
