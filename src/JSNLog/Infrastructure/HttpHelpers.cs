5a31ef71e1132ec6ef5e8d950c15eebfa665f882
Matt Perdeck <mperdeck@gmail.com>
2016-06-28 22:00:54 +1000

    In DNXCORE50, assume that charset of incoming json log entries is always utf-8

--- src/JSNLog/Infrastructure/HttpHelpers.cs
+++ src/JSNLog/Infrastructure/HttpHelpers.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+#if !DNXCORE50
 using System.Text.RegularExpressions;
+#endif
 
 #if NET40
 using System.Web;
@@ -14,8 +16,10 @@ namespace JSNLog.Infrastructure
 {
     internal static class HttpHelpers
     {
+#if !DNXCORE50
         private static Regex _regex = new Regex(@";\s*charset=(?<charset>[^\s;]+)");
-        
+#endif
+
         public static Encoding GetEncoding(string contentType)
         {
             if (string.IsNullOrEmpty(contentType))
@@ -23,12 +27,19 @@ namespace JSNLog.Infrastructure
                 return Encoding.UTF8;
             }
 
+            // Seeing that log entries are sent as JSON, the browser should always sending them
+            // in Unicode, probably UTF-8.
             string charset = "utf-8";
+
+#if !DNXCORE50
+            // Processing any charset anyway, just to be sure.
+            // But not for DNXCORE50, because that doesn't support regular expressions in RC2.
             var match = _regex.Match(contentType);
             if (match.Success)
             {
                 charset = match.Groups["charset"].Value;
             }
+#endif
 
             try
             {
