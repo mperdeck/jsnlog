8e89c70595d8a0c8506009585a1d9e6f18e5d7b6
Matt Perdeck <mperdeck@gmail.com>
2016-06-28 20:18:26 +1000

    DNXCORE no longer supports regular expressions, so do not check url params in dnxcore50

--- src/JSNLog/ValueInfos/UrlValue.cs
+++ src/JSNLog/ValueInfos/UrlValue.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+#if !DNXCORE50
 using System.Text.RegularExpressions;
+#endif
 using JSNLog.Exceptions;
 using JSNLog.Infrastructure;
 
@@ -7,7 +9,9 @@ namespace JSNLog.ValueInfos
 {
     internal class UrlValue : IValueInfo
     {
+#if !DNXCORE50
         private static Regex regexUrl = new Regex(Constants.RegexUrl);
+#endif
 
         Func<string, string> _virtualToAbsoluteFunc = null;
         public UrlValue(Func<string, string> virtualToAbsoluteFunc)
@@ -17,10 +21,12 @@ namespace JSNLog.ValueInfos
 
         public string ToJavaScript(string text)
         {
+#if !DNXCORE50
             if (!regexUrl.IsMatch(text))
             {
                 throw new InvalidAttributeException(text);
             }
+#endif
 
             string resolvedUrl = Utils.AbsoluteUrl(text, _virtualToAbsoluteFunc);
             return HtmlHelpers.JavaScriptStringEncode(resolvedUrl, true);
