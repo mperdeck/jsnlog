362e046434a1c710b4da9bc604b1a6854b3f17bf
Matt Perdeck <mperdeck@gmail.com>
2016-08-20 02:34:05 +1000

    Testsite project no longer needs access to inernals

--- src/JSNLog/Properties/AssemblyInfo.cs
+++ src/JSNLog/Properties/AssemblyInfo.cs
@@ -27,16 +27,8 @@ using System.Runtime.InteropServices;
 // http://stackoverflow.com/questions/36141302/why-is-it-recommended-to-include-the-private-key-used-for-assembly-signing-in-op
 [assembly: AssemblyKeyFileAttribute("../jsnlog.strongname.snk")]
 
-// 
-
-
+// Allow project JSNLog.Tests to access the internals of this project. Public key is needed because JSNLog has a strong name.
 [assembly: InternalsVisibleTo("JSNLog.Tests, PublicKey=" +
-    "002400000480000094000000060200000024000052534131000400000100010015488505fd9c86" +
-    "9dfb9af3ee7d980a24f9a379fbea34c6311b481f77688c74fae162335ae47a8ef800bfd83c1795" +
-    "97ab12c86278065bd9cbc1997863f5bc4a2f03b5a519e0b7097edb5649e7d982b94f7d6c8ef60d" +
-    "35f79aaf9785d1f79d5bc0c529edc38fb99dc88cb7475d32946286b0766b3bc32bc4bd9871768f" +
-    "70ea49ba")]
-[assembly: InternalsVisibleTo("JSNLog.TestSite, PublicKey=" +
     "002400000480000094000000060200000024000052534131000400000100010015488505fd9c86" +
     "9dfb9af3ee7d980a24f9a379fbea34c6311b481f77688c74fae162335ae47a8ef800bfd83c1795" +
     "97ab12c86278065bd9cbc1997863f5bc4a2f03b5a519e0b7097edb5649e7d982b94f7d6c8ef60d" +
