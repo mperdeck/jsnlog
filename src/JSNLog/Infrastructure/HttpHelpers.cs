a979fc31c4cbaececfe02c9c408e2ce60c1485e1
chikkannam <manjunath_tc@hotmail.com>
2016-08-31 18:48:08 -0400

    Update HttpHelpers.cs
    To Fix issue 115 https://github.com/mperdeck/jsnlog/issues/115
    This change will make sure the right client Ip address is passed along when the application is behind a proxy server

--- src/JSNLog/Infrastructure/HttpHelpers.cs
+++ src/JSNLog/Infrastructure/HttpHelpers.cs
@@ -43,9 +43,9 @@ namespace JSNLog.Infrastructure
         public static string GetUserIp(this HttpContext httpContext)
         {
 #if NET40
-            string userIp = httpContext.Request.UserHostAddress;
+            string userIp = (string.IsNullOrEmpty(HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]) ? httpContext.Request.UserHostAddress : HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
 #else
-            string userIp = Utils.SafeToString(httpContext.Connection.RemoteIpAddress);
+            string userIp = (string.IsNullOrEmpty(HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]) ? Utils.SafeToString(httpContext.Connection.RemoteIpAddress) : HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
 #endif
             return userIp;
         }
