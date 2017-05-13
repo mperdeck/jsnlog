e6b77f828abcd137cc2fcec21978610b449d680f
mperdeck <matt.perdeck@readify.net>
2017-05-13 18:56:17 +1000

    Initial core1, core-only test package

---
+++ jsnlog/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppLogger
+{
+    public class Logger
+    {
+        public static string Log(string text)
+        {
+            return text + " hello from core";
+        }
+    }
+}
+
