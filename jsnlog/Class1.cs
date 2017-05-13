a714d0bf73ba587bff93eb40987960de2b8a16c3
mperdeck <matt.perdeck@readify.net>
2017-05-13 23:37:00 +1000

    builds package for net452

--- jsnlog/Class1.cs
+++ jsnlog/Class1.cs
@@ -4,5 +4,14 @@ namespace jsnlog
 {
     public class Class1
     {
+        public static string Get(string s)
+        {
+#if NET452
+            string version = "NET452";
+#else
+            string version = "NETSTANDARD";
+#endif
+            return s + " " + version;
+        }
     }
 }
