2ef02a8b5e26c7764a39f71b22a9fda33c4957fe
mperdeck <matt.perdeck@readify.net>
2017-05-13 19:45:40 +1000

    net40 compile issues

---
+++ jsnlog/40/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppLogger40
+{
+    public class Logger
+    {
+        public static string Log(string text)
+        {
+            return text + " hello from core";
+        }
+    }
+}
+
---
+++ jsnlog/Core/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppLoggerCore
+{
+    public class Logger
+    {
+        public static string Log(string text)
+        {
+            return text + " hello from core";
+        }
+    }
+}
+
