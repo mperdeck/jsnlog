2714bcc3225d608326e7fba05b84969d8cbbf0cc
mperdeck <matt.perdeck@readify.net>
2017-05-14 14:28:13 +1000

    builds now

--- jsnlog/PublicFacing/AspNet5/TagHelpers/JlJavascriptLoggerDefinitionsTagHelper.cs
+++ jsnlog/PublicFacing/AspNet5/TagHelpers/JlJavascriptLoggerDefinitionsTagHelper.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
