84ede13d104f98749ff856d0f48b26542e3f30a0
mperdeck <black@hole.com>
2021-10-03 23:50:09 +1100

    add UseJSNLog overload for IApplicationBuilder

--- jsnlog/PublicFacing/AspNet5/Configuration/Middleware/ApplicationBuilderExtensions.cs
+++ jsnlog/PublicFacing/AspNet5/Configuration/Middleware/ApplicationBuilderExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
 
 namespace JSNLog
 {
@@ -15,12 +16,22 @@ namespace JSNLog
         /// </summary>
         /// <param name="builder"></param>
         /// <param name="loggingAdapter"></param>
-        /// <param name="jsnlogConfiguration"></param>
+        /// <param name="jsnlogConfiguration">
+        /// Note that if jsnlogConfiguration is set to null, the script tag and JavaScript config code will 
+        /// automatically be inserted in html responses.
+        /// </param>
         public static void UseJSNLog(this IApplicationBuilder builder,
             ILoggingAdapter loggingAdapter, JsnlogConfiguration jsnlogConfiguration = null)
         {
             JavascriptLogging.SetJsnlogConfiguration(jsnlogConfiguration, loggingAdapter);
             builder.UseMiddleware<JSNLogMiddleware>();
+        }
+
+        public static void UseJSNLog(this IApplicationBuilder builder,
+            ILoggerFactory loggerFactory, JsnlogConfiguration jsnlogConfiguration = null)
+        {
+            var loggingAdapter = new LoggingAdapter(loggerFactory);
+            UseJSNLog(builder, loggingAdapter, jsnlogConfiguration);
         }
     }
 }
