e89d5e7e648f411665a5a04b9124371e13050944
mperdeck <black@hole.com>
2021-10-04 14:11:25 +1100

    copy Generated and SiteConstants to jsnlog from website

---
+++ jsnlog/Infrastructure/Generated.cs
@@ -0,0 +1,13 @@
+// Generated.cs gets copied from Generated.cs.template
+// During the copy, 3.0.0 is replaced by the actual version.
+
+namespace WebSite.App_Code
+{
+    public static class Generated
+    {
+        public static string Version { get { return "3.0.0";  } }
+        public static string FrameworkVersion { get { return "2.30.0"; } }
+        public static string JSNLogJsVersion { get { return "2.30.0"; } }
+    }
+}
+
---
+++ jsnlog/Infrastructure/SiteConstants.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.App_Code
+{
+    // Copied from web site project.
+    //TODO: move web site project to jsnlog solution, have only one copy of this file
+    public static class SiteConstants
+    {
+        public static string CurrentVersion = Generated.Version;
+        public static string CurrentFrameworkVersion = Generated.FrameworkVersion;
+        public static string CurrentJSNLogJsVersion = Generated.JSNLogJsVersion;
+        public const string JsnlogJsFileSize = "2kb";
+        public const string HttpHeaderRequestIdName = "JSNLog-RequestId";
+        public const string GlobalMethodCalledAfterJsnlogJsLoaded = "__jsnlog_configure";
+        
+        public const string HandlerExtension = ".logger";
+        public const string DefaultDefaultAjaxUrl = "/jsnlog" + HandlerExtension;
+
+        public const string DemoAspNetCoreGithubUrl = "https://github.com/mperdeck/jsnlogSimpleWorkingDemos/tree/master/jsnlogSimpleWorkingDemos/JSNLogDemo_Core_NetCoreApp2";
+        public const string DemoGithubUrl = "https://github.com/mperdeck/jsnlogSimpleWorkingDemos/tree/master/jsnlogSimpleWorkingDemos";
+        public const string AngularJsDemoGithubUrl = "https://github.com/mperdeck/JSNLog.AngularJS";
+        public const string Angular2CoreDemoGithubUrl = "https://github.com/mperdeck/jsnlog.AngularCoreDemo";
+        public const string LicenceUrl = "https://github.com/mperdeck/jsnlog/blob/master/LICENSE.md";
+        public const string LicenceName = "MIT";
+        public const string CdnJsUrl = "https://cdnjs.com/libraries/jsnlog";
+        public static string CdnJsDownloadUrl = "https://cdnjs.cloudflare.com/ajax/libs/jsnlog/" + CurrentJSNLogJsVersion + "/jsnlog.min.js";
+        public static string CdnJsScriptTag = @"<script crossorigin=""anonymous"" src=""" + CdnJsDownloadUrl + @"""></script>";
+
+        // This causes NuGet to search for all packages with "JSNLog" - so the user will see
+        // JSNLog.NLog, etc. as well.
+        public const string NugetDownloadUrl = "https://www.nuget.org/packages?q=jsnlog";
+
+        public static string CurrentYear
+        {
+            get { return DateTime.Now.ToString("yyyy"); }
+        }
+
+        public static string DownloadLinkJsnlogJs 
+        {
+            get { return "https://raw.githubusercontent.com/mperdeck/jsnlog.js/master/jsnlog.min.js"; }
+        }
+    }
+}
+
