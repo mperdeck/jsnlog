69c7d62b1f5eb4a7d1ea4178adce645cfcd9991e
mperdeck <black@hole.com>
2022-01-18 22:43:56 +1100

    fixes

--- jsnlog/Infrastructure/ResponseStreamWrapper.cs
+++ jsnlog/Infrastructure/ResponseStreamWrapper.cs
@@ -14,7 +14,7 @@ using Microsoft.AspNetCore.Http;
 namespace JSNLog.Infrastructure
 {
     /// <summary>
-    /// Wraps the Response Stream to inject the given script into all HTML pages just before the </body> tag.
+    /// Wraps the Response Stream to inject the JSNLog config script into all HTML pages just before the </body> tag.
     ///
     /// This class based on 
     /// https://weblog.west-wind.com/posts/2020/Mar/29/Content-Injection-with-Response-Rewriting-in-ASPNET-Core-3x
@@ -23,19 +23,19 @@ namespace JSNLog.Infrastructure
     {
         private Stream _baseStream;
         private HttpContext _context;
-        private string _scriptToInject;
-
         private bool _isContentLengthSet = false;
 
-        public ResponseStreamWrapper(Stream baseStream, HttpContext context, string scriptToInject)
+        public ResponseStreamWrapper(Stream baseStream, HttpContext context)
         {
             _baseStream = baseStream;
             _context = context;
             CanWrite = true;
-            _scriptToInject = scriptToInject;
         }
 
-        public override void Flush() => _baseStream.Flush();
+        public override void Flush()
+        {
+            _baseStream.Flush();
+        }
 
         public override Task FlushAsync(CancellationToken cancellationToken)
         {
@@ -69,56 +69,71 @@ namespace JSNLog.Infrastructure
             IsHtmlResponse(forceReCheck: true);
         }
 
-#if !NETFRAMEWORK
         public override void Write(ReadOnlySpan<byte> buffer)
         {
             _baseStream.Write(buffer);
         }
-#endif
 
         public override void WriteByte(byte value)
         {
             _baseStream.WriteByte(value);
         }
 
-
         public override void Write(byte[] buffer, int offset, int count)
         {
             if (IsHtmlResponse())
             {
-                ScriptInjectionHelper.InjectScriptAsync(buffer, offset, count, _context, _baseStream, _scriptToInject)
+                ScriptInjectionHelper.InjectScriptAsync(buffer.AsMemory(offset, count), _context, _baseStream)
                                               .GetAwaiter()
                                               .GetResult();
             }
             else
-                _baseStream.Write(buffer, offset, count);
+            {
+                _baseStream?.Write(buffer, offset, count);
+            }
         }
 
-        public override async Task WriteAsync(byte[] buffer, int offset, int count,
+        public override Task WriteAsync(byte[] buffer, int offset, int count,
                                               CancellationToken cancellationToken)
+        {
+            return WriteAsync(buffer.AsMemory(offset, count), cancellationToken).AsTask();
+        }
+
+        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
         {
             if (IsHtmlResponse())
             {
-                await ScriptInjectionHelper.InjectScriptAsync(
-                    buffer, offset, count,
-                    _context, _baseStream, _scriptToInject);
+                await ScriptInjectionHelper.InjectScriptAsync(buffer, _context, _baseStream);
             }
             else
-                await _baseStream.WriteAsync(buffer, offset, count, cancellationToken);
+            {
+                if (_baseStream != null)
+                    await _baseStream.WriteAsync(buffer, cancellationToken);
+            }
         }
 
-
         private bool? _isHtmlResponse = null;
-        
 
         private bool IsHtmlResponse(bool forceReCheck = false)
         {
             if (!forceReCheck && _isHtmlResponse != null)
                 return _isHtmlResponse.Value;
 
+            // we need to check if the active request is still valid
+            // this can fail if we're in the middle of an error response
+            // or url rewrite in which case we can't intercept
+            if (_context?.Response == null)
+                return false;
+
+            // Requirements for script injection:
+            // * has to have result body
+            // * 200 or 500 response
+            // * text/html response
+            // * UTF-8 formatted (explicit or no charset)
+
             _isHtmlResponse =
                 _context.Response?.Body != null &&
-                _context.Response.StatusCode == 200 &&
+                (_context.Response.StatusCode == 200 || _context.Response.StatusCode == 500) &&
                 _context.Response.ContentType != null &&
                 _context.Response.ContentType.Contains("text/html", StringComparison.OrdinalIgnoreCase) &&
                 (_context.Response.ContentType.Contains("utf-8", StringComparison.OrdinalIgnoreCase) ||
@@ -134,27 +149,74 @@ namespace JSNLog.Infrastructure
             {
                 _context.Response.Headers.ContentLength = null;
                 _isContentLengthSet = true;
-            } 
-                
+            }
+
             return _isHtmlResponse.Value;
         }
 
-#if !NETFRAMEWORK
-        public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
-        {
-            return _baseStream.WriteAsync(buffer, cancellationToken);
-        }
-#endif
-
         protected override void Dispose(bool disposing)
         {
-            _baseStream?.Dispose();
+            //_baseStream?.Dispose();
             _baseStream = null;
             _context = null;
 
             base.Dispose(disposing);
         }
 
+        #region Byte Helpers
+        /// <summary>
+        /// Tries to find a
+        /// </summary>
+        /// <param name="buffer">byte array to be searched</param>
+        /// <param name="bufferToFind">byte to find</param>
+        /// <returns></returns>
+        public static int IndexOfByteArray(byte[] buffer, byte[] bufferToFind)
+        {
+            if (buffer.Length == 0 || bufferToFind.Length == 0)
+                return -1;
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                if (buffer[i] == bufferToFind[0])
+                {
+                    bool innerMatch = true;
+                    for (int j = 1; j < bufferToFind.Length; j++)
+                    {
+                        if (buffer[i + j] != bufferToFind[j])
+                        {
+                            innerMatch = false;
+                            break;
+                        }
+                    }
+                    if (innerMatch)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns an index into a byte array to find a string in the byte array.
+        /// Exact match using the encoding provided or UTF-8 by default.
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <param name="stringToFind"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
+        public static int IndexOfByteArray(byte[] buffer, string stringToFind, Encoding encoding = null)
+        {
+            if (encoding == null)
+                encoding = Encoding.UTF8;
+
+            if (buffer.Length == 0 || string.IsNullOrEmpty(stringToFind))
+                return -1;
+
+            var bytes = encoding.GetBytes(stringToFind);
+
+            return IndexOfByteArray(buffer, bytes);
+        }
+        #endregion
 
         public override bool CanRead { get; }
         public override bool CanSeek { get; }
--- jsnlog/Infrastructure/ScriptInjectionHelper.cs
+++ jsnlog/Infrastructure/ScriptInjectionHelper.cs
@@ -12,60 +12,70 @@ using Microsoft.AspNetCore.Http;
 /// </summary>
 namespace JSNLog.Infrastructure
 {
-
     public static class ScriptInjectionHelper
     {
+        private const string _jsnLogStartMarker = "<!-- JSNLog Configuration Start -->";
+        private const string _jsnLogEndMarker = "<!-- JSNLog Configuration End -->";
         private const string _bodyMarker = "</body>";
+
         private static readonly byte[] _bodyBytes = Encoding.UTF8.GetBytes(_bodyMarker);
+        private static readonly byte[] _markerBytes = Encoding.UTF8.GetBytes(_jsnLogStartMarker);
 
         /// <summary>
-        /// Adds script into the page before the body tag.
+        /// Adds a script block with JSNLog configuration code before the body tag.
         /// </summary>
         /// <param name="buffer"></param>
-        /// <param name="offset"></param>
-        /// <param name="count"></param>
         /// <param name="context"></param>
         /// <param name="baseStream">The raw Response Stream</param>
-        /// <param name="scriptToInject">
-        /// String to inject before the </body> tag
-        /// </param>
         /// <returns></returns>
-        public static Task InjectScriptAsync(byte[] buffer, int offset, int count, HttpContext context, Stream baseStream, string scriptToInject)
+        public static Task InjectScriptAsync(ReadOnlyMemory<byte> buffer, HttpContext context, Stream baseStream)
         {
-            Span<byte> currentBuffer = buffer;
-            var curBuffer = currentBuffer.Slice(offset, count).ToArray();
-            return InjectScriptAsync(curBuffer, context, baseStream, scriptToInject);
+            return InjectScriptAsync(buffer.ToArray(), context, baseStream);
         }
 
-        /// <summary>
-        /// Adds script into the page before the body tag.
-        /// </summary>
-        /// <param name="buffer"></param>
-        /// <param name="context"></param>
-        /// <param name="baseStream">The raw Response Stream</param>
-        /// <returns></returns>
-        public static async Task InjectScriptAsync(byte[] buffer, HttpContext context, Stream baseStream, string scriptToInject)
+        public static async Task InjectScriptAsync(byte[] buffer, HttpContext context, Stream baseStream)
         {
-            int index = buffer.LastIndexOf(_bodyBytes);
+            var index = buffer.LastIndexOf(_markerBytes);
+
+            if (index > -1)
+            {
+                await baseStream.WriteAsync(buffer, 0, buffer.Length);
+                return;
+            }
+
+            index = buffer.LastIndexOf(_bodyBytes);
             if (index == -1)
             {
                 await baseStream.WriteAsync(buffer, 0, buffer.Length);
                 return;
             }
 
+            var endIndex = index + _bodyBytes.Length;
+
             // Write pre-marker buffer
-            await baseStream.WriteAsync(buffer, 0, index);
+            await baseStream.WriteAsync(buffer, 0, index - 1);
 
             // Write the injected script
-            var scriptBytes = Encoding.UTF8.GetBytes(scriptToInject);
+            var scriptBytes = Encoding.UTF8.GetBytes(GetJsnLogConfigurationScript(context));
             await baseStream.WriteAsync(scriptBytes, 0, scriptBytes.Length);
 
             // Write the rest of the buffer/HTML doc
-            await baseStream.WriteAsync(buffer, index, buffer.Length - index);
+            await baseStream.WriteAsync(buffer, endIndex, buffer.Length - endIndex);
         }
 
         static int LastIndexOf<T>(this T[] array, T[] sought) where T : IEquatable<T> =>
             array.AsSpan().LastIndexOf(sought);
+
+        private static string GetJsnLogConfigurationScript(HttpContext context)
+        {
+            string script =
+                _jsnLogStartMarker +
+                context.Configure(null) +
+                _jsnLogEndMarker +
+                _bodyMarker;
+
+            return script;
+        }
     }
 }
 
