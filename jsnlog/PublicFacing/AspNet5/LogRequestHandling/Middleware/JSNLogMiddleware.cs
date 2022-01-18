12cfe4c6121326ddddefa0ff23352422a5c4d228
mperdeck <black@hole.com>
2022-01-18 22:43:24 +1100

    move log request processing to separate class

--- jsnlog/PublicFacing/AspNet5/LogRequestHandling/Middleware/JSNLogMiddleware.cs
+++ jsnlog/PublicFacing/AspNet5/LogRequestHandling/Middleware/JSNLogMiddleware.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Primitives;
 using JSNLog.Infrastructure;
 using JSNLog.LogHandling;
 using Microsoft.Extensions.Logging;
+using jsnlog.Infrastructure;
 
 #if !NETCORE2
 using Microsoft.AspNetCore.Http.Features;
@@ -40,7 +41,7 @@ namespace JSNLog
             string url = context.Request.GetDisplayUrl();
             if (LoggingUrlHelpers.IsLoggingUrl(url))
             {
-                await ProcessLoggerRequestAsync(context);
+                await LoggerRequestHelpers.ProcessLoggerRequestAsync(context, _logger);
                 return;
             }
 
@@ -79,113 +80,5 @@ namespace JSNLog
             }
 #endif
         }
-
-        private async Task ProcessLoggerRequestAsync(HttpContext context)
-        {
-            // If there is an exception whilst processing the log request (for example when the connection with the
-            // Internet disappears), try to log that exception. If that goes wrong too, fail silently.
-
-            try
-            {
-                await ProcessRequestAsync(context);
-            }
-            catch (Exception e)
-            {
-                try
-                {
-                    _logger.LogInformation($"JSNLog: Exception while processing log request -  {e}");
-                }
-                catch
-                {
-                }
-            }
-
-            return;
-        }
-
-        private async Task ProcessRequestAsync(HttpContext context)
-        {
-            var headers = ToDictionary(context.Request.Headers);
-            string urlReferrer = headers.SafeGet("Referer");
-            string url = context.Request.GetDisplayUrl();
-
-            var logRequestBase = new LogRequestBase(
-                userAgent: headers.SafeGet("User-Agent"),
-                userHostAddress: context.Wrapper().GetUserIp(),
-                requestId: context.Wrapper().GetLogRequestId(),
-                url: (urlReferrer ?? url).ToString(),
-                queryParameters: ToDictionary(context.Request.Query),
-                cookies: ToDictionary(context.Request.Cookies),
-                headers: headers);
-
-            DateTime serverSideTimeUtc = DateTime.UtcNow;
-            string httpMethod = context.Request.Method;
-            string origin = headers.SafeGet("Origin");
-
-            Encoding encoding = HttpHelpers.GetEncoding(headers.SafeGet("Content-Type"));
-
-            string json;
-            using (var reader = new StreamReader(context.Request.Body, encoding))
-            {
-                json = await reader.ReadToEndAsync();
-            }
-
-            var response = new LogResponse();
-
-            LoggerProcessor.ProcessLogRequest(json, logRequestBase,
-                serverSideTimeUtc,
-                httpMethod, origin, response);
-
-            // Send dummy response. That way, the log request will not remain "pending"
-            // in eg. Chrome dev tools.
-            //
-            // This must be given a MIME type of "text/plain"
-            // Otherwise, the browser may try to interpret the empty string as XML.
-            // When the user uses Firefox, and right clicks on the page and chooses "Inspect Element",
-            // then in that debugger's console it will say "no element found".
-            // See
-            // http://www.acnenomor.com/307387p1/how-do-i-setup-my-ajax-post-request-to-prevent-no-element-found-on-empty-response
-            // http://stackoverflow.com/questions/975929/firefox-error-no-element-found/976200#976200
-
-            ToAspNet5Response(response, context.Response);
-            context.Response.ContentType = "text/plain";
-            context.Response.ContentLength = 0;
-        }
-
-        private void ToAspNet5Response(LogResponse logResponse, HttpResponse owinResponse)
-        {
-            owinResponse.StatusCode = logResponse.StatusCode;
-
-            foreach (KeyValuePair<string, string> kvp in logResponse.Headers)
-            {
-                owinResponse.Headers[kvp.Key] = kvp.Value;
-            }
-        }
-
-        private Dictionary<string, string> ToDictionary(IEnumerable<KeyValuePair<string, string>> values)
-        {
-            var result = new Dictionary<string, string>();
-            foreach (var kvp in values)
-            {
-                result[kvp.Key] = kvp.Value.ToString();
-            }
-
-            return result;
-        }
-
-        private Dictionary<string, string> ToDictionary(IEnumerable<KeyValuePair<string, StringValues>> values)
-        {
-            var result = new Dictionary<string, string>();
-            foreach (var kvp in values)
-            {
-                result[kvp.Key] = kvp.Value.ToString();
-            }
-
-            return result;
-        }
-
-
-
-        
     }
 }
